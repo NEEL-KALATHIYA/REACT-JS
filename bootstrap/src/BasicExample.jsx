69b27efb3f2f54b8a1c3772587d8b668c0d3faac
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2024-09-27 15:39:23 +0530

    React App

---
+++ bootstrap/src/BasicExample.jsx
@@ -0,0 +1,38 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function BasicExample() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZTEyYzVjYjEtNWYzZi00NWRhLTgyZjUtNDlkZjBiNWIxYTEwXkEyXkFqcGc@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title>Stree 2</Card.Title>
+        <Button variant="primary">Book Now</Button>
+      </Card.Body>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZTEyYzVjYjEtNWYzZi00NWRhLTgyZjUtNDlkZjBiNWIxYTEwXkEyXkFqcGc@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title>Stree 2</Card.Title>
+        <Button variant="primary">Book Now</Button>
+      </Card.Body>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZTEyYzVjYjEtNWYzZi00NWRhLTgyZjUtNDlkZjBiNWIxYTEwXkEyXkFqcGc@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title>Stree 2</Card.Title>
+        <Button variant="primary">Book Now</Button>
+      </Card.Body>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZTEyYzVjYjEtNWYzZi00NWRhLTgyZjUtNDlkZjBiNWIxYTEwXkEyXkFqcGc@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title>Stree 2</Card.Title>
+        <Button variant="primary">Book Now</Button>
+      </Card.Body>
+      <Card.Img variant="top" src="https://m.media-amazon.com/images/M/MV5BZTEyYzVjYjEtNWYzZi00NWRhLTgyZjUtNDlkZjBiNWIxYTEwXkEyXkFqcGc@._V1_.jpg" />
+      <Card.Body>
+        <Card.Title>Stree 2</Card.Title>
+        <Button variant="primary">Book Now</Button>
+      </Card.Body>
+      
+    </Card>
+    
+  );
+}
+
+export default BasicExample;

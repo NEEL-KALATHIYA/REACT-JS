1c27d28d0f1df16866793a80c6ecec9bd4869719
NEEL-KALATHIYA <kalathiyaneel4@gmail.com>
2025-01-24 14:06:13 +0530

    api

---
+++ api/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import Form from "./Form";
+import Courses from "./Courses";
+import { Alert } from "@mui/material";
+
+const App = () => {
+  return (
+    <div>
+      <Form />
+      <Courses />
+    </div>
+  );
+};
+
+export default App;
